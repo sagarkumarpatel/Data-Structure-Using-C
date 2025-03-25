97cc85d202390bc8ebc4e8b0943a9fae4bc40038
sagarkumarpatel <sagarpatel98212@gmail.com>
2025-03-26 01:41:30 +0530

    implemented three function:- isempty, isfull and enqueue

--- Queue_Array_Implementation/Queue_Array_Implementation.c
+++ Queue_Array_Implementation/Queue_Array_Implementation.c
@@ -9,10 +9,45 @@ struct Queue{
 };
 struct Queue* createQueue(int size){
     struct Queue* q=(struct Queue*)malloc(sizeof(struct Queue));
-    q->font=-1;
-    q->rear=-1;
+    q->font=q->rear=-1;
     q->arr=(int*)malloc(size*sizeof(int));
     return q;
+}
+
+int isFull(struct Queue *q){
+    if(q->rear==q->size-1){
+        return 1;
+    }
+    else{
+        return 0;
+    }
+}
+
+int isEmpty(struct Queue *q){
+    if(q->font==-1){
+        return 1;
+    }
+    else{
+        return 0;
+    }
+}
+
+void enqueue(struct Queue* q, int data){
+    if(isFull(q)){
+        printf("Queue is full\n");
+        return;
+    }
+    else{
+        if(q->rear==-1){
+            q->font=0;
+            q->rear=0;
+            q->arr[q->rear]=data;
+        }
+        else{
+            q->rear++;
+            q->arr[q->rear]=data;
+        }
+    }
 }
 
 
